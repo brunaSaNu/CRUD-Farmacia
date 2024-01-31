a1f9fca9fbfaedbe968a7fee9660f775e7a697f7
BrunaSaNu <bruna.santana.0511@gmail.com>
2024-01-31 12:54:05 -0300

    Configurações iniciais

---
+++ src/main/java/com/generation/farmacita/FarmacitaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmacita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/farmacita/FarmacitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.farmacita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
